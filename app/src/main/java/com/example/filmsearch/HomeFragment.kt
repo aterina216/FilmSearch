94f393b6f6bd71fc41bb8869f67ee72b4efc1f6e
Katya <ater.93@mail.ru>
2025-03-21 22:29:42 +0300

    add circular animation from home_fragment

--- app/src/main/java/com/example/filmsearch/HomeFragment.kt
+++ app/src/main/java/com/example/filmsearch/HomeFragment.kt
@@ -109,15 +109,12 @@ class HomeFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         val searchSlide = Slide(Gravity.TOP).addTarget(R.id.search_view)
-//Создаем анимацию выезда RV снизу
         val recyclerSlide = Slide(Gravity.BOTTOM).addTarget(R.id.main_recycler)
-//Создаем экземпляр TransitionSet, который объединит все наши анимации
         val customTransition = TransitionSet().apply {
-            //Устанавливаем время, за которое будет проходить анимация
             duration = 500
-            //Добавляем сами анимации
             addTransition(recyclerSlide)
             addTransition(searchSlide)
+            AnimationHelper.performFragmentCircularrevealAnimation(view, requireActivity(), 1)
         }
 
         val recv = binding?.mainRecycler
